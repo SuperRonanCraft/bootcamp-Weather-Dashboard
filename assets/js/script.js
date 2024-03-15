2f91c8e6a9fe9ba650898368b5c48fcf706c754b
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-15 00:00:10 -0400

    weather response + search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,44 @@
+//Dependencies
 const inputSearch = $("#search-input");
+const autoBox = $("#autocomplete-box");
+const forecastEl = $("#forecast");
 const apiKey = "81f5de324935a1e9a4ba6383132f2d99";
 // const geocodeUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=8&appid=${apiKey}`;
 // const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 let slowedFetch = null;
 
+//Functions
 function fetchGeocode() {
   slowedFetch = null;
   const input = inputSearch.val();
+  if (input === "") return;
   const url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=8&appid=${apiKey}`;
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      autoBox.empty();
+      for (const loc of data) {
+        const locEl = $("<p>");
+        locEl.addClass("suggestion");
+        locEl.text(loc.name + " - " + loc.state);
+        const data = {
+          lon: loc.lon,
+          lat: loc.lat,
+          city: loc.name,
+          state: loc.state,
+        };
+        locEl.data("data", data);
+        autoBox.append(locEl);
+        // console.log(loc);
+      }
     });
+}
+
+function clearSuggestions() {
+  autoBox.empty();
+  inputSearch.val("");
 }
 
 function handleSearch(event) {
@@ -22,4 +46,69 @@ function handleSearch(event) {
   slowedFetch = setTimeout(fetchGeocode, 300);
 }
 
+function handleSuggestionClick(event) {
+  const target = $(event.target);
+  const data = target.data("data");
+  clearSuggestions();
+  getWeather(data);
+  addHistory(data);
+}
+
+function addHistory(data) {
+  const history = JSON.parse(localStorage.getItem("cityHistory")) || [];
+  history.push(data);
+  localStorage.setItem("cityHistory", JSON.stringify(history));
+}
+
+function getWeather(data) {
+  fetchForecast(data);
+  fetchWeather(data);
+}
+
+function fetchForecast(data) {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lang=en&lat=${data.lat}&lon=${data.lon}&appid=${apiKey}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((rData) => {
+      // console.log(rData.list);
+      const now = dayjs();
+      forecastEl.empty();
+      for (const weather of rData.list) {
+        const then = dayjs.unix(weather.dt);
+        if (then.hour() === 11) {
+          const weatherEl = $("<div>");
+          weatherEl.append($(`<h4>${then.format("MM/DD/YYYY")}</h4>`));
+          weatherEl.append($(`<p>Temp: ${weather.main.temp} °F</p>`));
+          weatherEl.append($(`<p>Wind: ${weather.wind.speed} MPH</p>`));
+          weatherEl.append($(`<p>Humidity: ${weather.main.humidity} %</p>`));
+          weatherEl.addClass(
+            "bg-dark text-light col-12 col-sm-6 col-md-4 col-lg-2"
+          );
+          forecastEl.append(weatherEl);
+        }
+      }
+    });
+}
+
+function fetchWeather(data) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lang=en&lat=${data.lat}&lon=${data.lon}&appid=${apiKey}`;
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((rData) => {
+      console.log(rData);
+      $("#city").text(rData.name);
+      $("#temp").text(`Temp: ${rData.main.temp} °F`);
+      $("#wind").text(`Wind: ${rData.wind.speed} MPH`);
+      $("#humidity").text(`Humidity: ${rData.main.humidity} %`);
+      // console.log(data);
+    });
+}
+
+//User Interactions
 inputSearch.on("input", handleSearch);
+inputSearch.on("search", clearSuggestions);
+autoBox.on("click", handleSuggestionClick);
